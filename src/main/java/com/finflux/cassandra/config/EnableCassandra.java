c8d2672094e2c196fd15faa82a1bb4cb3ecdfc7b
vamshivksp <vamshikrishna@confluxtechnologies.com>
2020-05-21 22:56:30 +0530

    MCS Cganges

--- src/main/java/com/finflux/cassandra/config/EnableCassandra.java
+++ src/main/java/com/finflux/cassandra/config/EnableCassandra.java
@@ -18,14 +18,14 @@
  */
 package com.finflux.cassandra.config;
 
-import org.springframework.context.annotation.Import;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
